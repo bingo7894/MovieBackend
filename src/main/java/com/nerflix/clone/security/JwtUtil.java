a32116ab541d9e7424413af52d4b9226eb90ce99
bingo7894 <pokpong.padjunreed@gmail.com>
2026-03-13 21:26:20 +0700

    feat: JwtAuthenication and JwtUtil

---
+++ src/main/java/com/nerflix/clone/security/JwtUtil.java
@@ -0,0 +1,77 @@
+package com.nerflix.clone.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    private  static final long JWT_TOKEN_VALIDITY = 24 * 60 * 60 * 1000;
+
+    @Value("${jwt.secret:defaultSecretKryForNetflixClone}")
+    private String secret;
+
+    private SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String getUsernameFromToken(String token){
+        return  getClaimFromToken(token,Claims::getSubject);
+    }
+
+    public String getRoleFromToken(String token){
+        return getClaimFromToken(token,claims -> claims.get("role",String.class));
+    }
+
+    public Date getExpirationDateFromToken(String token){
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims,T> claimResolver){
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token){
+        return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+    private Boolean isTokenExpired(String token){
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public  String generateToken(String username,String role){
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("role",role);
+        return doGenerateToken(claims,username);
+    }
+
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(subject)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public Boolean validateToken(String token){
+        try{
+            getAllClaimsFromToken(token);
+            return !isTokenExpired(token);
+        } catch (Exception e){
+            return false;
+        }
+    }
+}
